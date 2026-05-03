56cbd779fdda2053791ead77c4c21e5b9dc4ffa7
LAN233398 <520twc666@gmail.com>
2026-05-04 00:09:16 +0800

    Delete scripts directory

--- scripts/update_weekly_report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 立信小狐 周报自动更新脚本
-# 由 launchd 每周二调用
-
-export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:$HOME/bin:$PATH"
-cd "/Users/lanyijun/Library/Mobile Documents/com~apple~CloudDocs/lyj+claude"
-python3 weekly_report.py --push >> "运营数据/周报/cron_$(date +%Y%m%d).log" 2>&1
